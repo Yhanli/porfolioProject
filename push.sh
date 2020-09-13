3af4cbbb76b8e17e685d56fd5aa93202300ac1fd
Yhanli <45836848+Yhanli@users.noreply.github.com>
2020-09-14 09:53:36 +1200

    moved media_root_dist

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 sudo rm -R myPortfolio/media_root_dist
-sudo cp -R myPortfolio/media_root/ media_root_dist
+sudo cp -R myPortfolio/media_root/ myPortfolio/media_root_dist
 sudo cp myPortfolio/db.sqlite3 myPortfolio/db.sqlite3_dist
 sudo git add .
 sudo git commit
