dec7fc42fdea3af0fe3677949371e49fac33316e
yhanli <leeyuhan@qq.com>
2020-09-18 14:54:57 +1200

    added npm install and pipenv sync

--- pull.sh
+++ pull.sh
@@ -2,12 +2,14 @@
 sudo git stash
 sudo git reset --hard origin/master
 sudo git pull
+sudo npm install
 sudo rm -R myPortfolio/media_root
 sudo rm myPortfolio/db.sqlite3
 sudo cp -R myPortfolio/media_root_dist/ myPortfolio/media_root/
 sudo cp myPortfolio/db.sqlite3_dist myPortfolio/db.sqlite3
 sudo chmod -R 777 *
 sudo npm run build
+pipenv sync
 pipenv run python myPortfolio/manage.py makemigrations
 pipenv run python myPortfolio/manage.py migrate
 pipenv run python myPortfolio/manage.py collectstatic
